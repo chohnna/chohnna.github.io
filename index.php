c6bc5cb3c15156e97b1521f7487b53bff47097a5
Hanna Cho <70152437+hanncho@users.noreply.github.com>
2020-09-01 01:09:36 +0900

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,6 @@
 </div>
 <?php
 require('Persistence.php');
-
 $db = new Persistence();
 if( $db->add_comment($_POST) ) {
   header( 'Location: index.php' );
@@ -51,5 +50,42 @@ else {
   header( 'Location: index.php?error=Your comment was not posted due to errors in your form submission' );
 }
 ?>
+ 
+<?php
+require('Persistence.php');
+$comment_post_ID = 1;
+$db = new Persistence();
+$comments = $db->get_comments($comment_post_ID);
+$has_comments = (count($comments) > 0);
+?>
+
+ <input type="hidden" name="comment_post_ID" value="<?php echo($comment_post_ID); ?>" id="comment_post_ID" />
 </footer>
+<ol id="posts-list" class="hfeed<?php echo($has_comments?' has-comments':’); ?>">
+  <li class="no-comments">Be the first to add a comment.</li>
+  <?php
+    foreach ($comments as $comment) {
+      ?>
+      <li><article id="comment_<?php echo($comment['id']); ?>" class="hentry">  
+        <footer class="post-info">
+          <abbr class="published" title="<?php echo($comment['date']); ?>">
+            <?php echo( date('d F Y', strtotime($comment['date']) ) ); ?>
+          </abbr>
+
+          <address class="vcard author">
+            By <a class="url fn" href="#"><?php echo($comment['comment_author']); ?></a>
+          </address>
+        </footer>
+
+        <div class="entry-content">
+          <p><?php echo($comment['comment']); ?></p>
+        </div>
+      </article></li>
+      <?php
+    }
+  ?>
+</ol>
+#posts-list.has-comments li.no-comments {
+  display: none;
+}
 </html>
